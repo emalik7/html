6d3803af4c0c3c03fffc3155c3146331fd198658
Evel <cool.evel@mail.ru>
2022-07-07 15:10:36 +0300

    Initial commit

---
+++ assets/js/button_show_grammar.js
@@ -0,0 +1,20 @@
+var $reviewsSlider = $('.reviews-slider');
+if ($reviewsSlider.length) {
+  $reviewsSlider.slick({
+    accessibility: false,
+    centerMode: true,
+    slidesToShow: 5,
+    responsive: [{
+      breakpoint: 1200,
+      settings: {
+        slidesToShow: 3
+      }
+    }, {
+      breakpoint: 767,
+      settings: {
+        slidesToShow: 1,
+        centerMode: false,
+      }
+    }]
+  });
+}
